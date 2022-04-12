e320193002ad8f55a5370406c96b4cdd062f608b
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-04-12 11:24:55 +0530

    Created footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Login } from "./Pages/Index";
+import {Footer} from "./Components/Index"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
