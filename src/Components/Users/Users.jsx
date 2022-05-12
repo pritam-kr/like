1c048527c830c2525faa5330065e3f4800b51d5e
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-05-12 08:52:53 +0530

    Feat: Follow and unfollow, latest post of a particular user

---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,69 @@
+ 
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { followUser, unFollowUser } from "../../Store/Slice/UserSlice";
+import "./Users.css";
+
+const Users = ({ eachUser }) => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  const {
+    auth: { token, userInfo },
+    user: { users },
+  } = state;
+
+  const followHandler = (userId) => {
+    dispatch(followUser({ followUserId: userId, token: token }));
+  };
+
+  const unfollowHandler = (userId) => {
+    dispatch(unFollowUser({ unFollowUserId: userId, token: token }))
+  };
+
+   const currentUser = users.find((each) => each.username === userInfo.username)
+    
+
+  return (
+    <li className="my-2  py-2 " key={eachUser._id}>
+      <div className="admin-short-info border-b-0 bg-light-bg">
+        <div className="flex items-center">
+          <img
+            src="https://res.cloudinary.com/dhqxln7zi/image/upload/v1652266218/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws_o3oigd.jpg"
+            alt="users-avatar"
+            className="post-avatar"
+          />
+          <div className="ml-2 w-full">
+            <h1 className="post-user-name leading-none flex justify-between items-center ">
+              <p>
+                {eachUser.firstName} {eachUser.lastName}
+              </p> 
+              {currentUser.following.find((each) => each._id === eachUser._id) ? (
+                <button
+                  className="text-sm mr-2 btn-follow-unfollow rounded-full pt-2 pb-2 pr-3 pl-3"
+                  onClick={() => unfollowHandler(eachUser._id)}
+                >
+                   
+                  Following
+                </button>
+              ) : (
+                <button
+                  className="text-sm mr-2 btn-follow-unfollow rounded-full pt-2 pb-2 pr-3 pl-3"
+                  onClick={() => followHandler(eachUser._id)}
+                >
+                   
+                  Follow
+                </button>
+              )}
+            </h1>
+            <p className="text-[#909090]">{eachUser.username}</p>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export { Users };
+
+// <button className="text-sm mr-2 btn-follow-unfollow rounded-full pt-2 pb-2 pr-3 pl-3"> UnFollow</button>
--- src/Store/Slice/PostSlice.js
+++ src/Store/Slice/PostSlice.js
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import {getPosts} from "../../Services/index"
 
 const STATUS = Object.freeze({
   DEFAULT: false,
@@ -7,8 +8,27 @@ const STATUS = Object.freeze({
   ERROR: "",
 });
 
+export const getAllPost = createAsyncThunk("posts/getPosts", async (_, { rejectWithValue }) => {
+  try {
+    const {
+      data: { posts },
+      status,
+    } = await getPosts();
+
+    if (status === 200) {
+      return { posts: posts };
+    }
+  } catch (error) {
+    console.log(error);
+    return rejectWithValue("Try again later")
+  }
+});
+
 const initialState = {
+  allPost: [],
   posts: [],
+  loading: false,
+  error: "",
   status: STATUS.LOADING,
 };
 
@@ -16,7 +36,7 @@ const postSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {
-    getAllPost: (state, action) => {
+    userPosts: (state, action) => {
       state.posts = action.payload;
     },
 
@@ -33,30 +53,54 @@ const postSlice = createSlice({
     },
 
     getLikePost: (state, action) => {
-        console.log(action.payload)
-    }, 
-    getDislikePost: (state, action) => {
+      console.log(action.payload);
+    },
+    getDislikePost: (state, action) => {},
+  },
 
-    }
+  extraReducers: {
+
+    [getAllPost.pending] : (state) => {
+      state.loading = true
+
+    }, 
+    [getAllPost.fulfilled] : (state, action) => {
+      const {posts} = action.payload
+      state.loading = false
+      state.allPost = posts
+    }, 
+
+    [getAllPost.rejected] : (state) => {
+      state.loading = false
+      state.error = "Error occured! Try again later"
+    } 
+
   },
 });
 
-export const { getNewPost, getDeletePost, getAllPost, setStatus, getDislikePost, getLikePost } =
-  postSlice.actions;
+export const {
+  getNewPost,
+  getDeletePost,
+  userPosts,
+  setStatus,
+  getDislikePost,
+  getLikePost,
+} = postSlice.actions;
+
 export default postSlice.reducer;
 
 // Redux Toolkit Thunk
+//Getting all post of a particular user
 export const fetchPostData = (username) => {
   return async function fetchPostThunk(dispatch, state) {
     dispatch(setStatus(STATUS.LOADING));
     try {
       const { data, status } = await axios.get(`/api/posts/user/${username}`);
-
       const posts = data.posts;
 
       if (status === 200) {
         dispatch(setStatus(STATUS.DEFAULT));
-        dispatch(getAllPost(posts));
+        dispatch(userPosts(posts));
       }
     } catch (error) {}
   };
