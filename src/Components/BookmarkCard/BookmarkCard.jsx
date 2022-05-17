4aa9e83839ecdef12ae134b4db20bbac0a221a26
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-05-17 20:51:50 +0530

    Update BookmarkCard.jsx

--- src/Components/BookmarkCard/BookmarkCard.jsx
+++ src/Components/BookmarkCard/BookmarkCard.jsx
@@ -1,66 +1,105 @@
 import React from "react";
 import * as FaIcons from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { removeFromBookmark } from "../../Store/Slice/BookmarkSlice";
-import { likePost ,dislikePost} from "../../Store/Slice/PostSlice";
+import { likePost, dislikePost } from "../../Store/Slice/PostSlice";
 import { likeByUser } from "../../Utils/LikeByUser";
-import { Avatar } from "../Index";
 
-const BookmarkCard = ({eachPost}) => {
+const BookmarkCard = ({ eachPost }) => {
+  const navigate = useNavigate();
 
   const state = useSelector((state) => state);
-  const { auth: {token, userInfo}, user:{users}, post:{allPost} } = state;
-  const dispatch =  useDispatch()
+  const {
+    auth: { token, userInfo },
+    user: { users },
+    post: { allPost },
+  } = state;
+  const dispatch = useDispatch();
 
- const {_id, username, content, caption, likes:{likeCount}} = eachPost || {}
-  
-   
-  //Post Like Handler 
-  const likePostHandler = (_id, token) =>{
-      dispatch(likePost( {postId: _id, token: token}))
-  }
- 
+  const {
+    _id,
+    username,
+    content,
+    caption,
+    id,
+    likes: { likeCount },
+  } = eachPost || {};
+
+  //Post Like Handler
+  const likePostHandler = (_id, token) => {
+    dispatch(likePost({ postId: _id, token: token }));
+  };
+
   // Dislike Post Handler
   const disLikePostHandler = (_id, token) => {
-     dispatch(dislikePost({postId: _id, token: token}))
-  }
+    dispatch(dislikePost({ postId: _id, token: token }));
+  };
 
-   //Remove from bookmark 
-   const removeBookmarkHandler = (_id, token) => {
-    dispatch(removeFromBookmark({postId: _id, token: token}))
-}
+  //Remove from bookmark
+  const removeBookmarkHandler = (_id, token) => {
+    dispatch(removeFromBookmark({ postId: _id, token: token }));
+  };
 
+  //Single Post and comment handler
+  const singlePostHandler = (postId) => {
+    if (postId) {
+      navigate(`/post/${postId}`);
+    }
+  };
 
   return (
     <div className="feed-post-card mb-4 pd-0 rounded-3xl p-3 bg-light-bg text-[#fff]">
       <div className="flex items-center justify-between p-2">
         <div className="flex items-center ">
-           <Avatar username={username} />
+          <img
+            src={
+              "https://sbcf.fr/wp-content/uploads/2018/03/sbcf-default-avatar.png"
+            }
+            alt="admin"
+            className="post-avatar mr-3"
+          />
           <h1 className="post-user-name"> {username}</h1>
         </div>
       </div>
-
-     
-
       <div className="px-2 py-2">
         <div>
-          <p className="font-bold text-caption-title "> 
-          {caption} 
+          <p
+            className="font-bold text-caption-title cursor-pointer"
+            onClick={() => singlePostHandler(id)}
+          >
+            {caption}
           </p>
-          <p className="text-caption leading-6 ">
+          <p
+            className="text-caption leading-6 cursor-pointer"
+            onClick={() => singlePostHandler(id)}
+          >
             {content}
           </p>
         </div>
 
-      {/*Feeds icons section */}
+        {/*Feeds icons section */}
         <div className="flex justify-between mt-4">
           <ul className="flex justify-start items-center py-2">
             <li className="flex justify-start items-center mr-2">
-            { likeByUser(eachPost, userInfo) ? <FaIcons.FaHeart className="icons like-icon liked"  onClick={() =>  disLikePostHandler(_id, token)} /> : <FaIcons.FaHeart className="icons like-icon "  onClick={() => likePostHandler(_id, token)} />}
+              {likeByUser(eachPost, userInfo) ? (
+                <FaIcons.FaHeart
+                  className="icons like-icon liked"
+                  onClick={() => disLikePostHandler(_id, token)}
+                />
+              ) : (
+                <FaIcons.FaHeart
+                  className="icons like-icon "
+                  onClick={() => likePostHandler(_id, token)}
+                />
+              )}
               <span className="ml-1">{likeCount}</span>
             </li>
             <li className="flex justify-start items-center mr-2">
-              <FaIcons.FaComment className="icons comment-icon " />
+              <FaIcons.FaComment
+                className="icons comment-icon "
+                onClick={() => singlePostHandler(id)}
+              />
             </li>
             <li className="flex justify-start items-center mr-2">
               <FaIcons.FaShareAlt className="icons share-icon " />
@@ -69,14 +108,16 @@ const BookmarkCard = ({eachPost}) => {
 
           <ul className="flex">
             <li className="flex justify-start items-center mr-2">
-              <FaIcons.FaTrash className="icons share-icon " onClick={() =>  removeBookmarkHandler(_id, token)} />
+              <FaIcons.FaTrash
+                className="icons share-icon "
+                onClick={() => removeBookmarkHandler(_id, token)}
+              />
             </li>
-            
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export { BookmarkCard }
+export { BookmarkCard };
