5ea7cfda82a452b7aa6e9ec5c136cc03bc6edeb2
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-05-18 21:14:23 +0530

    Fix Logout page

--- src/Pages/AuthPage/Logout.jsx
+++ src/Pages/AuthPage/Logout.jsx
@@ -8,11 +8,11 @@ const Logout = () => {
       <div className="auth-wrapper min-h-screen flex justify-center items-center">
         <div className="w-full max-w-lg max-h-96">
           <div className="text-center text-xl logout-box">
-            <FaIcons.FaSignOutAlt className="icons logout-icons text-logout-logo mr-auto ml-auto mb-3" />
+            <FaIcons.FaSignOutAlt className="icons logout-icons text-logout-logo mr-auto ml-auto mb-3 text-[4rem] text-[#f7f7f7]" />
             <h3 className="text-2xl font-semibold">
               Oh no! You're leaving... Are you sure{" "}
             </h3>
-           <Link to="/"> <button className="btn btn-login mt-3 text-lg">Login</button></Link>
+           <Link to="/"> <button className="btn btn-login mt-3 text-lg rounded-full">Login</button></Link>
           </div>
         </div>
       </div>
